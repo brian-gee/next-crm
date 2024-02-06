c8a0fb16b7889e55d3575738b21693c80b0fed74
brian-gee <39035211+brian-gee@users.noreply.github.com>
2024-02-06 16:15:06 -0500

    tables set up with sample data

--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -1,3 +1,16 @@
+import { DataTable } from "@/components/ui/data-table";
+import { columns } from "./columns";
+import { payments } from "./data";
+
 export default function Page() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="flex flex-col items-center justify-center pt-20">
+        <h1 className="mb-10 text-6xl font-bold text-center">Clients</h1>
+      </div>
+      <div className="p-10">
+        <DataTable columns={columns} data={payments} />
+      </div>
+    </main>
+  );
 }
