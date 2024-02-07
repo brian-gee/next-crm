118370c8227e24f7b3daa6ed6396211d0bba5f4e
brian-gee <39035211+brian-gee@users.noreply.github.com>
2024-02-06 21:10:30 -0500

    updating spacing on titles

--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -14,8 +14,8 @@ export default function Page() {
 
   return (
     <main>
-      <div className="flex flex-col items-center justify-center pt-20">
-        <h1 className="mb-10 text-6xl font-bold text-center">Clients</h1>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-6xl font-bold text-center">Clients</h1>
       </div>
       <div className="p-10">
         {isLoading ? (
--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -5,8 +5,8 @@ import { payments } from "./data";
 export default function Page() {
   return (
     <main>
-      <div className="flex flex-col items-center justify-center pt-20">
-        <h1 className="mb-10 text-6xl font-bold text-center">Orders</h1>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-6xl font-bold text-center">Orders</h1>
       </div>
       <div className="p-10">
         <DataTable
